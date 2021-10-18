ad557e508b68e83d73681e5fbc1815b9baf90450
hajnallkaa <hajnallkaa@gmail.com>
2021-10-18 20:21:18 +0200

    Add: index.js file with list and add transactions, transactions.js to store data

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const pathToFile = __dirname + "/" + "transactions.json"
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+app.get('/listTransactions', function (req, res) {
+   let transactions = JSON.parse(fs.readFileSync(pathToFile));
+      res.end(JSON.stringify(transactions));
+})
+
+app.post('/addUser', function (req, res) {
+   // First read existing users.
+    let transactions = JSON.parse(fs.readFileSync(pathToFile));
+    transactions.push(req.body);
+
+    fs.writeFileSync(pathToFile, JSON.stringify(transactions, null, 2), (err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+    });
+
+    res.end(JSON.stringify(transactions)); 
+})
+
+var server = app.listen(3001, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
